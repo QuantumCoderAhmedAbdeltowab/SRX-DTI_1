8ea0b96012b7b6b6c797a6b7370e1c66e7d86aa7
QuantumCoderAhmedAbdeltowab <bdaltwaba226@gmail.com>
2024-10-25 18:00:14 +0300

    Update DTI_predictor.py

--- DTI prediction/DTI_predictor.py
+++ DTI prediction/DTI_predictor.py
@@ -1,7 +1,147 @@
-# XGBoost Classifiers for drug-target prediction (5-fold)
+# # XGBoost Classifiers for drug-target prediction (5-fold)
+
+# import pandas as pd
+# import numpy as np
+
+# from xgboost import XGBClassifier
+# from sklearn.model_selection import KFold
+# from sklearn.metrics import roc_curve, auc
+# import matplotlib.pyplot as plt
+# from itertools import cycle
+
+
+# # Balanced with One-SVM-US ############################################################################
+# df_final = pd.read_csv('df_final_NR_AB.csv')
+
+# #######################################################################################################
+
+# # Remove unnamed columns
+# df_final.drop(df_final.columns[df_final.columns.str.contains('unnamed', case=False)], axis=1, inplace=True)
+
+# y_final = df_final['label'].values
+# df_final.drop(['label', 'protein_no', 'drug_no'], axis=1, inplace=True)
+# #print(df_final.head())
+
+# #######################################################################################################
+# # Read selected feature list
+# df_features_final = pd.read_csv('subset_accuracy_NR_AB.csv')
+
+# #######################################################################################################
+
+# features_final = df_features_final.loc[0, 'Feature']
+# features_final = features_final.replace(" ", "")
+# features_list1 = list(features_final.split(','))
+# print(features_list1)
+# print(len(features_list1))
+
+# X_final = df_final[[c for c in df_final.columns if c in features_list1]].values
+# #######################################################################################################
+
+# # Balanced with random undersampling ##################################################################
+# df_rand = pd.read_csv('df_NR_AB_rand.csv')
+
+# #######################################################################################################
+
+# # Remove unnamed columns
+# df_rand.drop(df_rand.columns[df_rand.columns.str.contains('unnamed', case=False)], axis=1, inplace=True)
+
+# y_rand = df_rand['label'].values
+# df_rand.drop(['label', 'protein_no', 'drug_no'], axis=1, inplace=True)
+# #print(df_rand.head())
+
+# #######################################################################################################
+# # Read selected feature list
+# df_features_rand = pd.read_csv('subset_accuracy_NR_AB_rand.csv')
+
+# #######################################################################################################
+
+# features_rand = df_features_rand.loc[0, 'Feature']
+# features_rand = features_rand.replace(" ", "")
+# features_list2 = list(features_rand.split(','))
+# print(features_list2)
+# print(len(features_list2))
+
+# X_rand = df_rand[[c for c in df_rand.columns if c in features_list2]].values
+# #######################################################################################################
+
+# # prepare the cross-validation procedure
+# cv = KFold(n_splits=5, random_state=1, shuffle=True)
+
+
+# # Set up plotting area
+# plt.figure()
+# lw = 2
+
+# colors = cycle(['cyan', 'indigo', 'seagreen', 'yellow', 'blue', 'darkorange'])
+
+# ## Create a XGBoost Classifier##########################################################################
+# classifier_xgb = XGBClassifier(eval_metric='logloss', use_label_encoder=False)
+
+# ## Training XGBoost Classifier with 1SVM balanced ######################################################
+
+# mean_tpr = 0.0
+# mean_fpr = np.linspace(0, 1, 100)
+# i = 0
+# for (train, test), color in zip(cv.split(X_final, y_final), colors):
+#     probas_ = classifier_xgb.fit(X_final[train], y_final[train]).predict_proba(X_final[test])
+#     # Compute ROC curve and area the curve
+#     fpr, tpr, thresholds = roc_curve(y_final[test], probas_[:, 1])
+#     mean_tpr += np.interp(mean_fpr, fpr, tpr)
+#     mean_tpr[0] = 0.0
+#     roc_auc = auc(fpr, tpr)
+
+#     i += 1
+
+# mean_tpr /= cv.get_n_splits(X_final, y_final)
+# mean_tpr[-1] = 1.0
+# mean_auc = auc(mean_fpr, mean_tpr)
+# mean_auc = format(round(mean_auc, 2), ".2f")
+# plt.plot(mean_fpr, mean_tpr, label="With One-SVM-US, AUROC="+str(mean_auc))
+
+# ## Training XGBoost Classifier with Random balanced #####################################################
+
+# mean_tpr = 0.0
+# mean_fpr = np.linspace(0, 1, 100)
+# i = 0
+# for (train, test), color in zip(cv.split(X_rand, y_rand), colors):
+#     probas_ = classifier_xgb.fit(X_rand[train], y_rand[train]).predict_proba(X_rand[test])
+#     # Compute ROC curve and area the curve
+#     fpr, tpr, thresholds = roc_curve(y_rand[test], probas_[:, 1])
+#     mean_tpr += np.interp(mean_fpr, fpr, tpr)
+#     mean_tpr[0] = 0.0
+#     roc_auc = auc(fpr, tpr)
+
+#     i += 1
+
+# mean_tpr /= cv.get_n_splits(X_rand, y_rand)
+# mean_tpr[-1] = 1.0
+# mean_auc = auc(mean_fpr, mean_tpr)
+# mean_auc = format(round(mean_auc, 2), ".2f")
+# plt.plot(mean_fpr, mean_tpr, label="With Random sampling, AUROC="+str(mean_auc))
+
+
+# #######################################################################################################
+# plt.plot([0, 1], [0, 1], color="black", lw=lw, linestyle="--")
+# plt.xlim([0.0, 1.0])
+# plt.ylim([0.0, 1.05])
+# plt.xlabel("False Positive Rate")
+# plt.ylabel("True Positive Rate")
+
+# #plt.title("Group AB - EN")
+# #plt.title("Group AB - GPCR")
+# #plt.title("Group AB - IC")
+# plt.title("Group AB - NR")
+
+# plt.legend(loc="lower right")
+# plt.show()
+
 
 import pandas as pd
 import numpy as np
+import warnings  # For suppressing warnings
+
+# Suppress specific warnings from XGBoost
+warnings.filterwarnings(action='ignore', category=UserWarning, module='xgboost')
 
 from xgboost import XGBClassifier
 from sklearn.model_selection import KFold
@@ -9,9 +149,8 @@ from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 from itertools import cycle
 
-
 # Balanced with One-SVM-US ############################################################################
-df_final = pd.read_csv('df_final_NR_AB.csv')
+df_final = pd.read_csv('DTI prediction/df_final_NR_AB.csv')
 
 #######################################################################################################
 
@@ -20,7 +159,6 @@ df_final.drop(df_final.columns[df_final.columns.str.contains('unnamed', case=Fal
 
 y_final = df_final['label'].values
 df_final.drop(['label', 'protein_no', 'drug_no'], axis=1, inplace=True)
-#print(df_final.head())
 
 #######################################################################################################
 # Read selected feature list
@@ -38,7 +176,7 @@ X_final = df_final[[c for c in df_final.columns if c in features_list1]].values
 #######################################################################################################
 
 # Balanced with random undersampling ##################################################################
-df_rand = pd.read_csv('df_NR_AB_rand.csv')
+df_rand = pd.read_csv('DTI prediction/df_NR_AB_rand.csv')
 
 #######################################################################################################
 
@@ -47,11 +185,10 @@ df_rand.drop(df_rand.columns[df_rand.columns.str.contains('unnamed', case=False)
 
 y_rand = df_rand['label'].values
 df_rand.drop(['label', 'protein_no', 'drug_no'], axis=1, inplace=True)
-#print(df_rand.head())
 
 #######################################################################################################
 # Read selected feature list
-df_features_rand = pd.read_csv('subset_accuracy_NR_AB_rand.csv')
+df_features_rand = pd.read_csv('DTI prediction/subset_accuracy_NR_AB_rand.csv')
 
 #######################################################################################################
 
@@ -64,18 +201,16 @@ print(len(features_list2))
 X_rand = df_rand[[c for c in df_rand.columns if c in features_list2]].values
 #######################################################################################################
 
-# prepare the cross-validation procedure
+# Prepare the cross-validation procedure
 cv = KFold(n_splits=5, random_state=1, shuffle=True)
-
 
 # Set up plotting area
 plt.figure()
 lw = 2
-
 colors = cycle(['cyan', 'indigo', 'seagreen', 'yellow', 'blue', 'darkorange'])
 
 ## Create a XGBoost Classifier##########################################################################
-classifier_xgb = XGBClassifier(eval_metric='logloss', use_label_encoder=False)
+classifier_xgb = XGBClassifier(eval_metric='logloss', objective='binary:logistic')
 
 ## Training XGBoost Classifier with 1SVM balanced ######################################################
 
@@ -84,12 +219,11 @@ mean_fpr = np.linspace(0, 1, 100)
 i = 0
 for (train, test), color in zip(cv.split(X_final, y_final), colors):
     probas_ = classifier_xgb.fit(X_final[train], y_final[train]).predict_proba(X_final[test])
-    # Compute ROC curve and area the curve
+    # Compute ROC curve and area under the curve
     fpr, tpr, thresholds = roc_curve(y_final[test], probas_[:, 1])
     mean_tpr += np.interp(mean_fpr, fpr, tpr)
     mean_tpr[0] = 0.0
     roc_auc = auc(fpr, tpr)
-
     i += 1
 
 mean_tpr /= cv.get_n_splits(X_final, y_final)
@@ -105,12 +239,11 @@ mean_fpr = np.linspace(0, 1, 100)
 i = 0
 for (train, test), color in zip(cv.split(X_rand, y_rand), colors):
     probas_ = classifier_xgb.fit(X_rand[train], y_rand[train]).predict_proba(X_rand[test])
-    # Compute ROC curve and area the curve
+    # Compute ROC curve and area under the curve
     fpr, tpr, thresholds = roc_curve(y_rand[test], probas_[:, 1])
     mean_tpr += np.interp(mean_fpr, fpr, tpr)
     mean_tpr[0] = 0.0
     roc_auc = auc(fpr, tpr)
-
     i += 1
 
 mean_tpr /= cv.get_n_splits(X_rand, y_rand)
@@ -119,18 +252,12 @@ mean_auc = auc(mean_fpr, mean_tpr)
 mean_auc = format(round(mean_auc, 2), ".2f")
 plt.plot(mean_fpr, mean_tpr, label="With Random sampling, AUROC="+str(mean_auc))
 
-
 #######################################################################################################
 plt.plot([0, 1], [0, 1], color="black", lw=lw, linestyle="--")
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel("False Positive Rate")
 plt.ylabel("True Positive Rate")
-
-#plt.title("Group AB - EN")
-#plt.title("Group AB - GPCR")
-#plt.title("Group AB - IC")
 plt.title("Group AB - NR")
-
 plt.legend(loc="lower right")
 plt.show()
